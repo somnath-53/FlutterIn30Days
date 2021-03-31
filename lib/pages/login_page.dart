1d871ad27e654e3bc70f024ebfce6ec740bf0967
unknown <somnathgangdhar@gmail.com>
2021-03-31 18:48:57 +0530

    Day 6 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/utils/routes.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,115 +9,138 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState.validate()) {
+      Navigator.pushNamed(context, MyRoutes.homeRoute);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40.0,
-            ),
-            Image.asset(
-              "assets/images/image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome $name",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          autovalidateMode: (AutovalidateMode.onUserInteraction),
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40.0,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      hintText: "Enter E-Mail ID",
-                      labelText: "E-Mail ID",
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(
-                        () {},
-                      );
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                      side: BorderSide(color: Colors.black),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 138.0),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    child: Text(  
-                      "Sign In",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      side: BorderSide(color: Colors.black),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 95.0),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.signupRoute);
-                    },
-                    child: Text(
-                      "Create Account",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    ),
-                  ),
-                ],
+              Image.asset(
+                "assets/images/image.png",
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        // enabledBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.green),
+                        // ),
+                        // focusedBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.green),
+                        // ),
+                        hintText: "Enter E-Mail ID",
+                        labelText: "E-Mail ID",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator:
+                          EmailValidator(errorText: "Not a valid E-Mail"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(
+                          () {},
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        // enabledBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.green),
+                        // ),
+                        // focusedBorder: OutlineInputBorder(
+                        //   borderSide: BorderSide(color: Colors.green),
+                        // ),
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Password can not be empty";
+                        } else if (value.length < 6) {
+                          return "Password lenth should be atleast 6";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        side: BorderSide(color: Colors.black),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 138.0),
+                      ),
+                      onPressed: () => moveToHome(context),
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        side: BorderSide(color: Colors.black),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 95.0),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.signupRoute);
+                      },
+                      child: Text(
+                        "Create Account",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/utils/routes.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -7,132 +8,156 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  moveToLogin(BuildContext context) async {
+    if (_formKey.currentState.validate()) {
+      Navigator.pushNamed(context, MyRoutes.loginRoute);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 20.0,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Create an Account",
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green),
-                            ),
-                            hintText: "Enter Full Name",
-                            labelText: "Full Name"),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          hintText: "Enter E-Mail ID",
-                          labelText: "E-Mail ID",
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          hintText: "Enter Mobile Number",
-                          labelText: "Mobile Number",
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          hintText: "Enter Password",
-                          labelText: "Password",
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          hintText: "Password Again",
-                          labelText: "Confirm Password",
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.green,
-                          side: BorderSide(color: Colors.black),
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 138.0),
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.loginRoute);
-                        },
-                        child: Text(
-                          "Submit",
-                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+    return Form(
+      autovalidateMode: (AutovalidateMode.always),
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Create an Account",
+                        style: TextStyle(
+                          fontSize: 24,
                         ),
                       )
                     ],
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+
+                            // enabledBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            // focusedBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            hintText: "Enter Full Name",
+                            labelText: "Full Name",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            // enabledBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            // focusedBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            hintText: "Enter E-Mail ID",
+                            labelText: "E-Mail ID",
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator:
+                              EmailValidator(errorText: "Not a valid E-Mail"),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            // enabledBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            // focusedBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            hintText: "Enter Mobile Number",
+                            labelText: "Mobile Number",
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            // enabledBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            // focusedBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            hintText: "Enter Password",
+                            labelText: "Password",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            // enabledBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            // focusedBorder: OutlineInputBorder(
+                            //   borderSide: BorderSide(color: Colors.green),
+                            // ),
+                            hintText: "Password Again",
+                            labelText: "Confirm Password",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                            side: BorderSide(color: Colors.black),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 138.0),
+                          ),
+                          onPressed: () => moveToLogin(context),
+                          child: Text(
+                            "Submit",
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
