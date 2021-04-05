00f35fca6cee7bb8f84132b8ce29d10b9cd09d8b
unknown <somnathgangdhar@gmail.com>
2021-04-05 11:02:34 +0530

    Day 12 Completed

--- lib/widgets/items_widget.dart
+++ lib/widgets/items_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/models/catalog.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ItemWidget extends StatelessWidget {
   final Item item;
@@ -11,6 +12,14 @@ class ItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () {
+          Fluttertoast.showToast(
+              msg: item.name,
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.cyan[200],
+              textColor: Colors.black);
+        },
         leading: Image.network(item.imageUrl),
         title: Text(item.name),
         subtitle: Text(item.display),
